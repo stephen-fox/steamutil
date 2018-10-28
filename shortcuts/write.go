96ec1f99127cdd355d98b47aa30ddb61b36c021f
Stephen Fox <stephen.j.fox.jr@gmail.com>
2018-10-28 12:49:54 -0400

    Added 'OverwriteVdfV1File()'.

--- shortcuts/write.go
+++ shortcuts/write.go
@@ -2,9 +2,29 @@ package shortcuts
 
 import (
 	"io"
+	"os"
 
 	"github.com/stephen-fox/steamutil/vdf"
 )
+
+func OverwriteVdfV1File(f *os.File, scs []Shortcut) error {
+	_, err := f.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+
+	err = f.Truncate(0)
+	if err != nil {
+		return err
+	}
+
+	err = WriteVdfV1(scs, f)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func WriteVdfV1(shortcuts []Shortcut, w io.Writer) error {
 	config := vdf.Config{
